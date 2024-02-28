a7fc0ffd38decfd7cc55ebc7a67541601964a412
NTSP3 <ntos32bit@gmail.com>
2024-02-28 22:21:16 +0530

    Added 'whatsNew'

--- src/kernel/apps/builtins.h
+++ src/kernel/apps/builtins.h
@@ -8,8 +8,12 @@ int colorMain(int argc, char **argv);
 
 int cat(int argc, char **argv);
 
+int clear(int argc, char **argv);
+
 int lsMain(int argc, char **argv);
 
 int mkdir(int argc, char **argv);
 
 int help(int argc, char **argv);
+
+int whatsNew(int argc, char **argv);
