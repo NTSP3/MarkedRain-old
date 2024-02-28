0463fdda59a4905e8f893552eef27f0aaebeba71
NTSP3 <ntos32bit@gmail.com>
2024-02-28 22:35:06 +0530

    Updated help

--- src/kernel/apps/help.c
+++ src/kernel/apps/help.c
@@ -1,20 +1,24 @@
+#include "shell.h"
 #include <stdio.h>
 
 int help(__attribute__((unused)) int argc, __attribute__((unused)) char **argv)
 {
     // TODO : When processes : List also apps in fs
-
-    puts("\t\tOs2020 - v0.2");
-    puts("\tTo have more help on a specific command, type <command> --help");
-    puts("\tCommands :");
-    puts("- cat\tConcatenate files");
-    puts("- cd\tChange directory (only for shell)");
-    puts("- color\tSets the color of the background / foreground of the console");
-    puts("- echo\tDisplays arguments on stdout");
-    puts("- exit\tTerminate shell (only for shell)");
-    puts("- help\tDisplay all commands");
-    puts("- ls\tList directory");
-    puts("- mkdir\tCreate an empty directory");
+    
+    puts("\n OS Codename \"MarkedRain\"");
+    puts(" For more help on a specific command, type <cmd> --help (only for some cmds)");
+    puts("  -cat      :Concatenate files");
+    puts("  -cd       :Change directory (only for shell)");
+    puts("  -clear    :Clears tdhe screen");
+    puts("  -color    :Sets the color of the background / foreground of the console");
+    puts("  -echo     :Displays arguments on stdout");
+    puts("  -exit     :Terminate shell (only for shell)");
+    puts("  -help     :Display all commands");
+    puts("  -ls       :List directory");
+    puts("  -mkdir    :Create an empty directory");
+    puts("  -ver      :Shows current OS Version");
+    puts("  -whatsnew :Shows the new stuff introduced in the current and prior versions\n");
+    puts(" Also there's one new hidden command I will not list here.\n");
 
     return 0;
 }
